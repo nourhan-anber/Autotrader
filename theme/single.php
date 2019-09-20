8bce7cd4d7e9bd5060b21cf87c1ac1e406cc0fb0
Nourhan Anber <nourhan.anber@gmail.com>
2019-09-20 15:32:46 -0400

    single theme styling

--- theme/single.php
+++ theme/single.php
@@ -10,8 +10,8 @@ get_header();
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
- <?php the_title(); ?>
- <?php the_content(); ?>
+ <h1><?php the_title(); ?></h1>
+ <p><?php the_content(); ?></p>
  <?php echo get_the_date(); ?>
 
 <?php endwhile; ?>
